6f818447375855888ac85add155db5d9a6fa6ffa
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-08-15 10:46:33 +0600

    header navigation added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
 
 function App() {
   return (
     <div className="App">
-      <h2 className="text-primary">Hello world</h2>
+      <TopNavigation />
     </div>
   );
 }
---
+++ src/components/TopNavigation/TopNavigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const TopNavigation = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Fashion Paradise</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#deets">Home</Nav.Link>
+            <Nav.Link href="#deets">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default TopNavigation;
