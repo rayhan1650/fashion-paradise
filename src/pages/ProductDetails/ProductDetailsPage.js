935ab9e9de61b140b405b97e1944d3d156483c43
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-08-15 22:13:52 +0600

    form added to product details page

--- src/pages/ProductDetails/ProductDetailsPage.js
+++ src/pages/ProductDetails/ProductDetailsPage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Container, Row, Col, Card, Badge } from "react-bootstrap";
+import { Container, Row, Form, Button } from "react-bootstrap";
 import { GlassMagnifier } from "react-image-magnifiers";
 
 const ProductDetailsPage = () => {
   return (
     <Container>
-      <Row>
-        <div className="card my-5 shadow">
+      <Row className="mx-1">
+        <div className="card my-5 p-0 shadow">
           <div className="row g-0">
             <div className="col-md-6">
               <GlassMagnifier
@@ -16,21 +16,65 @@ const ProductDetailsPage = () => {
                 imageAlt="Example"
               />
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 bg-dark text-white">
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h4 className="card-title text-info">
+                  McLaren 2022 summer new men's t-shirts
+                </h4>
                 <p className="card-text">
                   This is a wider card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
                   longer.
                 </p>
-                <p className="card-text">
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
+                <ul>
+                  <li>100% cotton</li>
+                  <li>Size: M, L, XL, 2XL</li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
+      </Row>
+      <Row>
+        <h2 className="text-center">Submit your info for placing order</h2>
+        <Form>
+          {/* product Name  */}
+          <Form.Group className="mb-3" controlId="formBasicProductName">
+            <Form.Label>Product Name</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+
+          {/* product quantity  */}
+          <Form.Group className="mb-3" controlId="formBasicProductQuantity">
+            <Form.Label>Product Quantity</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter quantity of product"
+            />
+          </Form.Group>
+
+          {/* product size  */}
+          <Form.Group className="mb-3" controlId="formBasicProductSize">
+            <Form.Label>Product size</Form.Label>
+            <Form.Control type="text" placeholder="Enter size of product" />
+          </Form.Group>
+
+          {/* customer email  */}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          {/* customer phone no  */}
+          <Form.Group className="mb-3" controlId="formBasicPhoneNo">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="number" placeholder="Enter your phone number" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Place Order
+          </Button>
+        </Form>
       </Row>
     </Container>
   );
